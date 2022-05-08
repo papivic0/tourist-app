2cd6f160585699d0fbd3b12a53781b28eeab71b7
Victor Omoniyi <vomoniyi2001@gmail.com>
2022-05-08 21:26:22 +0100

    refactor

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const userRouter = require('./routes/userRoutes');
 const app = express();
 
 app.use(express.json());
-app.use(express.static(`${__dirname}/public`));
+app.use(express.static(`${__dirname}/public`)); 
 
 app.use((req, res, next) =>{
     console.log("This is the Middleware");
